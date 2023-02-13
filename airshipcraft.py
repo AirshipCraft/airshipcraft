57b4eb2f2a260335f42f4b25c9352b9523daa4ee
inspector-packet <brockq@infowest.com>
2023-02-13 04:33:36 -0700

    Add: /latency

--- airshipcraft.py
+++ airshipcraft.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from dotenv import load_dotenv
 import os
 
@@ -10,10 +11,17 @@ class aclient(discord.Client):
         intents.message_content = True
         super().__init__(intents = intents)
 
+        self.synced = False
+
     async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync(guild = discord.Object(id=os.getenv('GUILD_ID')))
+            self.synced = True
         print(f'{self.user} successfully logged in')
 
 client = aclient()
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_message(message):
@@ -21,5 +29,9 @@ async def on_message(message):
     channel = str(message.channel.name)
     user_message = str(message.content)
     print(f'Message {user_message} by {username} on {channel}')
+
+@tree.command(guild = discord.Object(id=os.getenv('GUILD_ID')), name = 'latency', description='Check the Bot Latency to Discord')
+async def latency(interaction: discord.Interaction):
+    await interaction.response.send_message(f'Server Ping: {round(client.latency * 1000)}ms')
 
 client.run(os.getenv('TOKEN'))
