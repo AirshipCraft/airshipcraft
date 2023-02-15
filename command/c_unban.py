817904fa0b496c5e3ac99d271e94160faebf24c7
inspector-packet <brockq@infowest.com>
2023-02-14 22:48:27 -0700

    Add: simple /unban

---
+++ command/c_unban.py
@@ -0,0 +1,7 @@
+async def unban(interaction, member, reason):
+    if not member.guild_permissions.ban_members:
+        await interaction.response.send_message("You do not have permission to use this command")
+        return
+
+    await interaction.guild.unban(member)  
+    await interaction.response.send_message(f"{member.mention} has been un-banned\n\nReason -> {reason}")
