15ab1363af12bb41e3c8c31d45862e45749fceae
inspector-packet <brockq@infowest.com>
2023-02-14 22:49:26 -0700

    Add: simple /mute

---
+++ command/c_mute.py
@@ -0,0 +1,17 @@
+import discord
+
+async def mute(interaction, member, reason):
+    if not member.guild_permissions.mute_members:
+        await interaction.response.send_message("You do not have permission to use this command")
+        return
+
+    muted_role = discord.utils.get(interaction.guild.roles, name="Muted")
+    
+    if not muted_role:
+        muted_role = await interaction.guild.create_role(name="Muted")
+        
+        for channel in interaction.guild.channels:
+            await channel.set_permissions(muted_role, send_messages=False)
+    
+    await member.add_roles(muted_role)
+    await interaction.response.send_message(f"{member.mention} has been muted\n\nReason -> {reason}")
