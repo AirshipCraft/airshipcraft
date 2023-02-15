5bfbb813d4e601c987e98d81de4d06c8657717b1
inspector-packet <brockq@infowest.com>
2023-02-14 22:46:09 -0700

    Fix: removed old files

--- command_airshipcraft.py
+++
@@ -1,2 +0,0 @@
-async def airshipcraft(interaction):
-    await interaction.response.send_message(f'This is still under Development')
--- command_latency.py
+++
@@ -1,2 +0,0 @@
-async def latency(interaction, client):
-    await interaction.response.send_message(f'Server Ping: {round(client.latency * 1000)}ms')
