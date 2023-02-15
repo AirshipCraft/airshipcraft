b2ec251baa803efd9663cbcde4781242ea39aafd
inspector-packet <brockq@infowest.com>
2023-02-14 22:48:56 -0700

    Add: simple /kick

---
+++ command/c_kick.py
@@ -0,0 +1,7 @@
+async def kick(interaction, member, reason):
+    if not member.guild_permissions.kick_members:
+        await interaction.response.send_message("You do not have permission to use this command")
+        return
+
+    await member.kick()
+    await interaction.response.send_message(f"{member.mention} has been kicked\n\nReason -> {reason}")
