4437bfb19389143a4063bd07c371a30150dd2fe9
inspector-packet <brockq@infowest.com>
2023-02-18 19:35:01 -0700

    Add: discord server member count

--- command/c_airshipcraft.py
+++ command/c_airshipcraft.py
@@ -5,4 +5,7 @@ async def airshipcraft(interaction):
     e.title = 'AirshipCraft Information'
     e.description = f'This is still under development'
     e.color = discord.Color.red()
+
+    e.add_field(name='Total Users', value=f'{interaction.guild.member_count}')
+
     await interaction.response.send_message(embed=e)
